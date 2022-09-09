1952a0c84b3997e459738a2ef4cbd17b5a313484
Koolrisk <fitz27gerald1022@gmail.com>
2022-09-09 14:03:53 -0700

    task 1 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Ruturn: Alwaya 0 (Success)
+ */
+int main(void)
+{
+        int n;
+
+        for (n = 0; n < 10; n++)
+               printf("%d", n);
+        putchar(10);       
+        return (0);
+
+}
