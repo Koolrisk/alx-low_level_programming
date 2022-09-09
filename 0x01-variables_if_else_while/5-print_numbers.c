d83706bff903f90b3d84862de3d5a9ee07dbbf52
Koolrisk <fitz27gerald1022@gmail.com>
2022-09-09 14:15:17 -0700

    task 5 and 100

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,16 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Ruturn: Alwaya 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = 0; n < 10; n++)
-               printf("%d", n);
-        putchar(10);       
-        return (0);
-
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	putchar(10);
+	return (0);
 }
